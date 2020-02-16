9477c106fa1c5efe38fb45c2af04c44ae102bc74
Yuki SUGAWARA <yxuxkxi_shsf@icloud.com>
2020-02-16 09:19:12 +0900

    escでゲーム終了を実装完了
    全てのシーンでescapeを押すことにより，ゲームを終了させることができるようにしました．

---
+++ Assets/Scripts/QuitGame.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitGame : MonoBehaviour
+{
+    void Quit() 
+    {
+        Application.Quit ();
+        UnityEditor.EditorApplication.isPlaying = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey (KeyCode.Escape)) {
+            Quit ();
+        }
+    }
+}
