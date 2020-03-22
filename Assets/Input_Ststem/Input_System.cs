199d128942c12e336e9bfc8da22a9e120368722b
hikky31 <53403515+hikky31@users.noreply.github.com>
2020-03-23 00:39:37 +0900

    hikky
    コード書き換えた.
    よくわかんない.

--- Assets/Input_Ststem/Input_System.cs
+++ Assets/Input_Ststem/Input_System.cs
@@ -8,7 +8,7 @@ using Random = UnityEngine.Random;
 public class Input_System : MonoBehaviour
 {
     //　文字列のパターン
-    private string[] qJ  = {"abyxybax", "ybaxxabx", "yaabxyab","bxayaxyb","yxbyxbax"};
+    private string[] qJ  = {"","a","b","x","y"};
    
     //　文字列表示
     private Text UIJ;
@@ -22,19 +22,21 @@ public class Input_System : MonoBehaviour
     //何文字目か
     private int index;
     private Text UIJC;
+
+    private int count = 0;
+    private bool Input_Flag = true;
     void Start()
-    {
+    { 
         //　テキストUIを取得
         UIJ = transform.Find("InputObject/Text").GetComponent<Text>();
-        
         OutPut();
-
+        
     }
 
     void OutPut()
     {
         //　問題数内でランダムに選ぶ
-        num = Random.Range(0, qJ.Length);
+        num = Random.Range(1, qJ.Length);
  
         //　選択した問題をテキストUIにセット
         nQJ = qJ[num];
@@ -45,35 +47,53 @@ public class Input_System : MonoBehaviour
 
     void Update()
     {
-        //　キーを押しているかどうか
-        if(Input.anyKeyDown
-           && (!Input.GetMouseButton(0) && !Input.GetMouseButton(1) && !Input.GetMouseButton(2))
-        ) {
-            //　今見ている文字とキーボードから打った文字が同じかどうか
-            if(Input.GetKeyDown(nQJ[index].ToString())) {
-                //　正解時の処理を呼び出す
-                Correct();
-            } else {
-                //　失敗時の処理を呼び出す
-                Mistake();
+        if (count != 8 && Input_Flag)
+        {
+            //　キーを押しているかどうか
+            if (Input.anyKeyDown
+                && (!Input.GetMouseButton(0) && !Input.GetMouseButton(1) && !Input.GetMouseButton(2))
+            )
+            {
+                //　今見ている文字とキーボードから打った文字が同じかどうか
+
+
+                if (Input.GetKeyDown(nQJ[index].ToString()))
+                {
+                    count++;
+                    OutPut();
+                }
+                else
+                {
+                    //　失敗時の処理を呼び出す
+                    Mistake();
+                }
+
             }
         }
-        
-        //　タイピング正解時の処理
+        else if(count == 8)
+        {
+            Correct();
+        }
+
+    }
+
+    
+
+    //　タイピング正解時の処理
         void Correct()
         {
-            index++;
-            if(index >= nQJ.Length)
-            {
-                Debug.Log("攻撃させていただきますわwwwww");
-            }
+            Debug.Log("攻撃させていただきますわwwwww");
+            count = 0;
+            UIJ.text = qJ[0];
+            Input_Flag = false;
         }
- 
+
         //　タイピング失敗時の処理
-        void Mistake() {
+        void Mistake()
+        {
             Debug.Log("ミスってるでwwwww");
         }
+
     }
-}
 
 
