b2ac73cd21bd8797f52b8b7cd4cd95b4131138a9
paradaisuikezaki <53808542+paradaisuikezaki@users.noreply.github.com>
2020-03-27 23:38:43 +0900

    add file
    add Gfile

---
+++ Assets/Scripts/PlayerControllerScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerScript : MonoBehaviour
+{
+
+    Rigidbody2D rigid2D;
+    Animator animator;
+    float walk = 10.0f;
+    float maxSpeed = 2.0f;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.rigid2D = GetComponent<Rigidbody2D>();
+        this.animator = GetComponent<Animator>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int key = 0;
+
+        //プレイヤの速度
+        float speedx = Mathf.Abs(this.rigid2D.velocity.x);
+
+        //スピード制限
+        if (speedx < this.maxSpeed)
+        {
+            this.rigid2D.AddForce(transform.right * key * this.walk);
+
+        }
+
+        //プレイヤの速度に応じてアニメーション速度を変える
+        this.animator.speed = speedx / 2.0f;
+    }
+}
+
+    
