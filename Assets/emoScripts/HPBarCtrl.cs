04c657ff7023014ff77a33b39bca8dfb8302ed88
Yuki SUGAWARA <yxuxkxi_shsf@icloud.com>
2020-03-31 18:36:05 +0900

    自身のシーン追加およびDOTweenの追加
    HPバーのアニメーション試したけど全然できなかったよ！！！

---
+++ Assets/emoScripts/HPBarCtrl.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HPBarCtrl : MonoBehaviour
+{
+    Slider _slider;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _slider = GameObject.Find ("Slider").GetComponent<Slider>();
+    }
+
+    float _hp = 1;
+    // Update is called once per frame
+    void Update()
+    {
+        _hp -= 0.01f;
+        if (_hp < 0) {
+            _hp = 1;
+        }
+
+        _slider.value = _hp;
+    }
+}
