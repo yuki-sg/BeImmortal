8d7e5dd8fada918f0807361b6ded4dd2e9388b52
paradaisuikezaki <53808542+paradaisuikezaki@users.noreply.github.com>
2021-04-17 14:01:24 +0900

    a

--- Assets/Scripts/PlayerControllerScript.cs
+++ Assets/Scripts/PlayerControllerScript.cs
@@ -4,46 +4,32 @@ using UnityEngine;
 
 public class PlayerControllerScript : MonoBehaviour
 {
-
-    Rigidbody2D rigid2D;
-    Animator animator;
-    float walk = 10.0f;
-    float maxSpeed = 2.0f;
-
+    private Animator anim;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        this.rigid2D = GetComponent<Rigidbody2D>();
-        this.animator = GetComponent<Animator>();
+        anim = gameObject.GetComponent<Animator>();
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        int key = 0;
-
-        //プレイヤの速度
-        float speedx = Mathf.Abs(this.rigid2D.velocity.x);
-
-        //スピード制限
-        if (speedx < this.maxSpeed)
+        if (Input.GetKey(KeyCode.Space))
         {
-            this.rigid2D.AddForce(transform.right * key * this.walk);
-
+            anim.SetBool("blwalk", true);
         }
-        /*
-        if()
+        else if (Input.GetKey(KeyCode.F1))
         {
-            this.animator.SetTrigger("AttackTrigger");
-
+            anim.SetBool("blattack", false);
         }
-        */
-        //プレイヤの速度に応じてアニメーション速度を変える
-        this.animator.speed = speedx / 2.0f;
+ 
     }
 }
+
+
+   
 
     
