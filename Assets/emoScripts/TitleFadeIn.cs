e713e26175c87387afec169ec57fc59cd602f0ee
Yuki S <eimoxshsf@gmail.com>
2021-04-20 05:30:29 +0900

    本環境での各種動作実装完了
    チュートリアル，1st，2ndでゲームシステム実装
    リザルトもキーボードで済ませられるように修正
    フェードイン，フェードアウトの微調整

--- Assets/emoScripts/TitleFadeIn.cs
+++ Assets/emoScripts/TitleFadeIn.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class FadeIn : MonoBehaviour
+public class TitleFadeIn : MonoBehaviour
 {
     // フェードインのおおよその秒数
     [SerializeField]
@@ -17,28 +17,23 @@ public class FadeIn : MonoBehaviour
         image = transform.Find("Panel").GetComponent<Image>();
         // コルーチンで使用する待ち時間を計測
         fadeInTime = 1f * fadeInTime / 100f;
+        StartCoroutine("FadeIn");
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        
     }
 
-    IEnumerator fadeIn()
+    IEnumerator FadeIn()
     {
-        yield return new WaitForSeconds(1.7f);
         // Colorのアルファを0.1ずつ下げていく
-        for (var i = 1f; i >= 0; i -= 0.01f)
+        for(var i = 1f; i >= 0; i -= 0.01f)
         {
             image.color = new Color(0.1176471f, 0.1176471f, 0.1176471f, i);
             // 指定秒数待つ
             yield return new WaitForSeconds(fadeInTime);
         }
-    }
-
-    public void fade_In()
-    {
-        StartCoroutine("fadeIn");
     }
 }
