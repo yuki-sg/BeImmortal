f3b40cc73086d0a7d024ad6977c367f5ba6015a5
paradaisuikezaki <53808542+paradaisuikezaki@users.noreply.github.com>
2021-04-24 01:26:36 +0900

    a

--- Assets/Scripts/PlayerControllerScript.cs
+++ Assets/Scripts/PlayerControllerScript.cs
@@ -5,18 +5,24 @@ using UnityEngine;
 public class PlayerControllerScript : MonoBehaviour
 {
     private Animator anim;
+    GameObject Player;
+    GameObject Enemy;
 
 
     // Start is called before the first frame update
     void Start()
     {
+        /*
         anim = gameObject.GetComponent<Animator>();
-
+        */
+        this.Player = GameObject.Find("Player");
+        this.Enemy = GameObject.Find("Enemy");
     }
 
     // Update is called once per frame
     void Update()
     {
+        /*
         if (Input.GetKey(KeyCode.Space))
         {
             anim.SetBool("blwalk", true);
@@ -25,7 +31,15 @@ public class PlayerControllerScript : MonoBehaviour
         {
             anim.SetBool("blattack", false);
         }
- 
+ */
+
+        //当たり判定
+        Vector2 p1 = this.Enemy.transform.position;
+        Vector2 p2 = this.Player.transform.position;
+        Vector2 dir = p1 - p2;
+        float d = dir.magnitude;
+        float r1 = 1.0f;
+        float r2 = 1.0f;
     }
 }
 
