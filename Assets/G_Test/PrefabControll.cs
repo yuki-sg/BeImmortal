3fdc2c3a727e16013519addc0466afccaae884db
paradaisuikezaki <53808542+paradaisuikezaki@users.noreply.github.com>
2021-04-25 18:14:19 +0900

    add animation for change Scene
    sorry

---
+++ Assets/G_Test/PrefabControll.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrefabControll : MonoBehaviour
+{
+    [SerializeField] GameObject Cube;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine("SpriteCount");
+    }
+
+    IEnumerator SpriteCount()
+    {
+        int count = 0;
+        int wide = 0;
+
+        for (count = 0; count < 4; count++)
+        {
+            for (wide = 0; wide < 4; wide++)
+            {
+
+
+                yield return new WaitForSeconds(1.5f);
+                Instantiate(Cube, new Vector2(wide, count), Quaternion.identity);
+            }
+
+
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
